7e571d82d7b83d6f879b58bf669de6e1e7edf545
yawar2019 <yawarali17@gmail.com>
2022-09-10 21:26:26 +0530

    Committed

--- AdoDotNet/Controllers/EmployeeController.cs
+++ AdoDotNet/Controllers/EmployeeController.cs
@@ -15,5 +15,73 @@ namespace AdoDotNet.Controllers
         {
             return View(db.GetAllEmployees());
         }
+
+        //going to be call at the time loading our Create page
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Create(EmployeeModel emp)//FormCollection frm
+        {
+            //string EmpName = frm["EmpName"];
+            //int EmpSalary = Convert.ToInt32(frm["EmpSalary"]);
+
+            int i = db.SaveEmployee(emp);
+
+            if (i > 0)
+            {
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int? id)
+        {
+            EmployeeModel emp = db.GetEmployeeById(id);
+            return View(emp);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(EmployeeModel emp)//FormCollection frm
+        {
+            //string EmpName = frm["EmpName"];
+            //int EmpSalary = Convert.ToInt32(frm["EmpSalary"]);
+
+            int i = db.UpdateEmployee(emp);
+
+            if (i > 0)
+            {
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Delete(int? id)
+        {
+            EmployeeModel emp = db.GetEmployeeById(id);
+            return View(emp);
+        }
+
+        [HttpPost]
+        [ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int? id)//FormCollection frm
+        {
+            int i = db.DeleteEmployee(id);
+
+            if (i > 0)
+            {
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
     }
 }
--- AdoDotNet/Models/EmployeeContext.cs
+++ AdoDotNet/Models/EmployeeContext.cs
@@ -32,5 +32,68 @@ namespace AdoDotNet.Models
             }
             return listemp;
         }
+
+        public int SaveEmployee(EmployeeModel emp)
+        {
+            SqlCommand cmd = new SqlCommand("sp_CreateEmployee", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            con.Open();
+            cmd.Parameters.AddWithValue("@EmpName", emp.EmpName);
+            cmd.Parameters.AddWithValue("@EmpSalary", emp.EmpSalary);
+            int i = cmd.ExecuteNonQuery();
+            con.Close();
+
+            return i;
+        }
+        
+        public EmployeeModel GetEmployeeById(int? id)
+        {
+            EmployeeModel emp = new Models.EmployeeModel();
+
+            SqlCommand cmd = new SqlCommand("sp_RajagetEmployeeById", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@EmpId", id);
+
+            DataTable dt = new DataTable();//
+
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(dt);
+
+            foreach (DataRow dr in dt.Rows)
+            {
+                emp.EmpId = Convert.ToInt32(dr["EmpId"]);
+                emp.EmpName = Convert.ToString(dr["EmpName"]);
+                emp.EmpSalary = Convert.ToInt32(dr["EmpSalary"]);
+
+            }
+            return emp;
+        }
+
+        //
+
+        public int UpdateEmployee(EmployeeModel emp)
+        {
+            SqlCommand cmd = new SqlCommand("spr_updateEmployeeDetails", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            con.Open();
+            cmd.Parameters.AddWithValue("@EmpId", emp.EmpId);
+            cmd.Parameters.AddWithValue("@EmpName", emp.EmpName);
+            cmd.Parameters.AddWithValue("@EmpSalary", emp.EmpSalary);
+            int i = cmd.ExecuteNonQuery();
+            con.Close();
+
+            return i;
+        }
+        public int DeleteEmployee(int? id)
+        {
+            SqlCommand cmd = new SqlCommand("usp_DeleteEmployeeById", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            con.Open();
+            cmd.Parameters.AddWithValue("@EmpId", id);
+            int i = cmd.ExecuteNonQuery();
+            con.Close();
+
+            return i;
+        }
     }
 }
