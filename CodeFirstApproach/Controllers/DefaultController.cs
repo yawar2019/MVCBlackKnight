ee5401c79aaf12f27b676c5657aca8cbb885818f
yawar2019 <yawarali17@gmail.com>
2022-09-24 21:32:47 +0530

    committed

---
+++ CodeFirstApproach/Controllers/DefaultController.cs
@@ -0,0 +1,55 @@
+﻿using CodeFirstApproach.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CodeFirstApproach.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        EmployeeContext db = new EmployeeContext();
+        public ActionResult Index()
+        {
+            return View(db.EmployeeModels.ToList());
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(EmployeeModel emp)
+        {
+            db.EmployeeModels.Add(emp);//insert Query Generated Here
+            int i=db.SaveChanges();//Execute Query
+            if(i>0)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public ActionResult Edit(int? id)
+        {
+            EmployeeModel emp = db.EmployeeModels.Find(id);
+            return View(emp);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(EmployeeModel emp)
+        {
+            db.Entry(emp).State=EntityState.Modified;//insert Query Generated Here
+            int i = db.SaveChanges();//Execute Query
+            if (i > 0)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
+}
