e41746f58b012938327334a969195a2a9ac9472b
yawar2019 <yawarali17@gmail.com>
2022-09-30 21:31:26 +0530

    committed

--- MVCBlackKnight/Models/EmployeeModel.cs
+++ MVCBlackKnight/Models/EmployeeModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -9,6 +10,8 @@ namespace MVCBlackKnight.Models
     {
         public int EmpId { get; set; }
         public string EmpName { get; set; }
+        [ScaffoldColumn(false)]
         public int EmpSalary { get; set; }
+        public bool status { get; set; }
     }
 }
